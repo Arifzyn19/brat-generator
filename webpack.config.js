124873254472cab8e09a4eb18f7f350a55824ea6
Arifzyn <arifzynapis19@gmail.com>
2025-07-21 11:44:10 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,11 @@ module.exports = (env, argv) => {
   return {
     entry: "./src/index.js",
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: isProduction ? "[name].[contenthash].js" : "[name].js",
-      clean: true,
-    },
+  path: path.resolve(__dirname, "dist"),
+  filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+  clean: true,
+  publicPath: "/brat-generator/",
+},
 
     devServer: {
       static: {
